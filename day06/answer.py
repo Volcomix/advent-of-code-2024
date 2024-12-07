f8ea4c396bf23b2195cadf3e1378d022ea799bd6
Volcomix <Volcomix@users.noreply.github.com>
2024-12-07 12:02:03 +0100

    Very slow solution for day 6 part 2

--- day06/answer.py
+++ day06/answer.py
@@ -26,3 +26,27 @@ while True:
     position = (row, col)
     visited.add(position)
 print(len(visited))
+
+loop_count = 0
+for i, obstruction in enumerate(visited):
+    path = [[(0, 0) for cell in row] for row in grid]
+    position = start_position
+    direction = start_direction
+    path[position[0]][position[1]] = direction
+    while True:
+        row, col = position
+        row += direction[0]
+        col += direction[1]
+        if not (0 <= row < len(grid) and 0 <= col < len(grid[row])):
+            break
+        if path[row][col] == direction:
+            loop_count += 1
+            break
+        cell = grid[row][col]
+        if cell == "#" or (row, col) == obstruction:
+            direction = (direction[1], -direction[0])
+            continue
+        position = (row, col)
+        path[position[0]][position[1]] = direction
+    print(f"{i} / {len(visited)}")
+print(loop_count)
